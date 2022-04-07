adc6d39b40a0fcb486c49ae3818c6126424b8f4d
Matheus Gomes <math.s.gomes@gmail.com>
2022-04-07 17:02:46 -0300

    Menu mobile adicionado

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,7 +1,13 @@
-const nav = document.querySelector('nav');
+const menuHamburguer = document.querySelector('#menu-hamburguer');
 const menu_mobile = document.querySelector('.menu__mobile');
 
-nav.addEventListener('click', () => {
-  menu_mobile.classList.toggle('ativo');
-  console.log(nav);
+menuHamburguer.addEventListener('click', () => {
+  menu_mobile.classList.add('ativo');
+})
+
+
+const closeButton = document.querySelector('#close-button');
+
+closeButton.addEventListener('click', (event) => {
+  event.target.parentElement.classList.remove('ativo');
 })
