79209b8880154b6f2ecd9f4784d99d1bc6dc5322
Matheus Gomes <math.s.gomes@gmail.com>
2022-04-13 16:25:59 -0300

    Finalização validação - Botão de submit só é ativado se todos os campos estiverem preenchidos

--- assets/js/validacao-formulario.js
+++ assets/js/validacao-formulario.js
@@ -3,32 +3,40 @@ const textarea = document.querySelector('textarea');
 
 const form = document.forms;
 const formElements = Array.from(form[0].elements);
+const lastElement = formElements.length - 1;
+const inputSubmit = formElements[lastElement];
 
-formElements.forEach((input) => {
+let formularioTodoPreenchido = false;
 
-  input.addEventListener('blur', (event) => {
-    const inputTipo = input.dataset.tipo;
-    const inputMaxLength = input.maxLength;
-    const inputMinLength = input.minLength;
-    const span = input.parentElement.querySelector('span');
+inputSubmit.disabled = true;
 
-    if(input.validity.valid) {
+// console.log(inputSubmit.disabled);
+
+formElements.forEach((campo) => {
+  const inputTipo = campo.dataset.tipo;
+  const inputMaxLength = campo.maxLength;
+  const inputMinLength = campo.minLength;
+  const span = campo.parentElement.querySelector('span');
+
+  campo.addEventListener('blur', (event) => {
+
+    if(campo.validity.valid) {
 
       span.style.display = 'none';
-      input.classList.remove("campo--invalido");
-      
-    } else if(input.value == '') {
+      campo.classList.remove("campo--invalido");
+
+    } else if(campo.value == '') {
       
       span.style.display = 'block';
       span.innerHTML = `O campo de ${inputTipo} não pode ficar vazio.`;
-      input.classList.add("campo--invalido");
+      campo.classList.add("campo--invalido");
 
-    } else if(input.value.length >= inputMaxLength) {
+    } else if(campo.value.length >= inputMaxLength) {
 
       span.style.display = 'block';
       span.innerHTML = `O campo de ${inputTipo} não pode ter mais do que ${inputMaxLength} caracteres.`;
 
-    } else if(input.value.length <= inputMinLength) {
+    } else if(campo.value.length <= inputMinLength) {
 
       span.style.display = 'block';
       span.innerHTML = `Esse campo não pode ter menos do que ${inputMinLength} caracteres.`;
@@ -38,9 +46,37 @@ formElements.forEach((input) => {
       span.style.display = 'block';
       span.innerHTML = 'Campo Inválido';
 
-      if(input.dataset.tipo == 'email') {
+      if(campo.dataset.tipo == 'email') {
         span.innerHTML = 'Por favor insira um endereço de email válido.';
       }
     }
+
+    // Função que verifica se todos os campos foram preenchidos.
+    formElements.forEach((campo) => {
+      if (formElements[0].validity.valid &&
+        formElements[1].validity.valid &&
+        formElements[2].validity.valid &&
+        formElements[3].validity.valid) {
+          
+        console.log("Todos os campos são validados");
+        formularioTodoPreenchido = true;
+      } else {
+
+        console.log("Há um ou mais campos inválidos");
+        formularioTodoPreenchido = false;
+      }
+    })
+
+    // Função que habilita ou desabilita o botão de envio
+    if(formularioTodoPreenchido) {
+
+      inputSubmit.disabled = false;
+    } else {
+
+      inputSubmit.disabled = true;
+    }
+
   })
+
 })
+
